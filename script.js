7eddcb5b075d6fe8647252735d83fdd69c288919
ChirCayo <134942070+ChirCayo@users.noreply.github.com>
2023-06-11 22:39:37 +0700

    2 files modified

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-let slides = document.getElementsByClassName("carousel-slide");
-let indicators = document.getElementsByClassName("carousel-indicator");
+let slides = document.getElementsByClassName('carousel-slide');
+let indicators = document.getElementsByClassName('carousel-indicator');
 let currentSlide = 0;
 
 function showSlide(slideIndex) {
   // Hide all slides
   for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = 'none';
   }
-  
+
   // Deactivate all indicators
   for (let i = 0; i < indicators.length; i++) {
-    indicators[i].classList.remove("active");
+    indicators[i].classList.remove('active');
   }
-  
+
   // Show the selected slide and activate the corresponding indicator
-  slides[slideIndex].style.display = "block";
-  indicators[slideIndex].classList.add("active");
+  slides[slideIndex].style.display = 'block';
+  indicators[slideIndex].classList.add('active');
   currentSlide = slideIndex; // Update the currentSlide variable
 }
 
@@ -45,12 +45,17 @@ function goToSlide(index) {
 setInterval(nextSlide, 3000);
 
 // Event listeners for prev/next buttons
-document.querySelector(".carousel-prev").addEventListener("click", previousSlide);
-document.querySelector(".carousel-next").addEventListener("click", nextSlide);
+document
+  .querySelector('.carousel-prev')
+  .addEventListener('click', previousSlide);
+document.querySelector('.carousel-next').addEventListener('click', nextSlide);
 
 // Event listeners for indicators
 for (let i = 0; i < indicators.length; i++) {
-  indicators[i].addEventListener("click", function() {
+  indicators[i].addEventListener('click', function () {
     goToSlide(i);
   });
 }
+
+// Inisialisasi tampilan awal
+showSlide(0);
