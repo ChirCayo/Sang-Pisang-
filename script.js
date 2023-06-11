072e90955835f5e0c7a35fd5d78bb1427cc85d41
ChirCayo <134942070+ChirCayo@users.noreply.github.com>
2023-06-11 22:29:19 +0700

    3 files modified

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
-let slideIndex = 0;
-showSlides();
+let slides = document.getElementsByClassName("carousel-slide");
+let indicators = document.getElementsByClassName("carousel-indicator");
+let currentSlide = 0;
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
+function showSlide(slideIndex) {
+  // Hide all slides
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  
+  // Deactivate all indicators
+  for (let i = 0; i < indicators.length; i++) {
+    indicators[i].classList.remove("active");
+  }
+  
+  // Show the selected slide and activate the corresponding indicator
+  slides[slideIndex].style.display = "block";
+  indicators[slideIndex].classList.add("active");
+}
+
+function nextSlide() {
+  currentSlide++;
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  showSlide(currentSlide);
+}
+
+function previousSlide() {
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  }
+  showSlide(currentSlide);
+}
+
+function goToSlide(index) {
+  if (index >= 0 && index < slides.length) {
+    currentSlide = index;
+    showSlide(currentSlide);
+  }
+}
+
+// Automatic slide change every 3 seconds
+setInterval(nextSlide, 3000);
+
+// Event listeners for prev/next buttons
+document.querySelector(".carousel-prev").addEventListener("click", previousSlide);
+document.querySelector(".carousel-next").addEventListener("click", nextSlide);
+
+// Event listeners for indicators
+for (let i = 0; i < indicators.length; i++) {
+  indicators[i].addEventListener("click", function() {
+    goToSlide(i);
+  });
 }
